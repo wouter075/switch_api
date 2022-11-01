83f224a8f7cbe4a92d3b088ba4f16434d6361ea5
Wouter <wouter075@gmail.com>
2022-11-01 10:47:57 +0100

    Added new route for the last 100 speeds from a certain port.

--- app.py
+++ app.py
@@ -1,23 +1,75 @@
-from flask import Flask
-import serial
+from flask import Flask, jsonify
+import sqlite3
+import collections
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Sorry, no endpoints here.'
 
 
-@app.route('/login/')
-def login():
-    # ser = serial.Serial('COM3', 38400, timeout=0, parity = serial.PARITY_EVEN, rtscts = 1)
-    ser = serial.Serial('COM3')
-    ser.write(b'\n')
-    ser.write(b'\n')
-    ser.write(b'\n')
-    s = ser.read()
+@app.route('/ports')
+def ports():
+    try:
+        sqlite_connection = sqlite3.connect('sw.db')
+        cursor = sqlite_connection.cursor()
+        q1 = "SELECT DISTINCT port FROM sw_data;"
+        cursor.execute(q1)
+        all_ports = cursor.fetchall()
+        cursor.close()
+        sqlite_connection.close()
 
-    return s
+    except sqlite3.Error as error:
+        all_ports = [f'{error}']
+
+    return jsonify(all_ports)
+
+@app.route('/speed/<port>')
+def speed_port(port):
+    try:
+        sqlite_connection = sqlite3.connect('sw.db')
+        cursor = sqlite_connection.cursor()
+        q1 = f"SELECT * FROM sw_data WHERE port = 'Gi1/0/{port}' ORDER BY timestamp DESC LIMIT 1;"
+        cursor.execute(q1)
+        port_speed = cursor.fetchall()
+        cursor.close()
+        sqlite_connection.close()
+
+    except sqlite3.Error as error:
+        port_speed = [f'{error}']
+
+    return jsonify(port_speed)
+
+@app.route('/speed/<port>/last')
+def speed_port_last(port):
+    try:
+        sqlite_connection = sqlite3.connect('sw.db')
+        sqlite_connection.row_factory = dict_factory
+
+        cursor = sqlite_connection.cursor()
+        q1 = f"SELECT * FROM sw_data WHERE port = 'Gi1/0/{port}' ORDER BY timestamp DESC LIMIT 100;"
+        cursor.execute(q1)
+        port_speed = cursor.fetchall()
+        cursor.close()
+        sqlite_connection.close()
+
+    except sqlite3.Error as error:
+        port_speed = [f'{error}']
+
+    response = jsonify(port_speed)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 if __name__ == '__main__':
